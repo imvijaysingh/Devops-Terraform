1d1d595fd08f535e2ea2bccda5b1df80fcff6d7f
imvijaysingh <59255347+imvijaysingh@users.noreply.github.com>
2020-09-07 18:10:46 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "key_name"{}
+variable "region"{
+    default = "ap-south-1"
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
